c035ea04097c9e388b753072f0bdad360f81d61f
Anar Agaev <anar.n.agaev@gmail.com>
2023-09-29 21:24:54 +0300

    Added mobile navigation for new technolight layout

---
+++ technolight.layout/js/pages/home.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// home.js
+
+const home = (value) => {
+    let pi = value
+    pi = pi === 3.14 ? true : false
+}
+
+document.addEventListener('loaded', () => home(3.14))
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJob21lL3NjcmlwdC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBob21lLmpzXHJcblxyXG5jb25zdCBob21lID0gKHZhbHVlKSA9PiB7XHJcbiAgICBsZXQgcGkgPSB2YWx1ZVxyXG4gICAgcGkgPSBwaSA9PT0gMy4xNCA/IHRydWUgOiBmYWxzZVxyXG59XHJcblxyXG5kb2N1bWVudC5hZGRFdmVudExpc3RlbmVyKCdsb2FkZWQnLCAoKSA9PiBob21lKDMuMTQpKSJdLCJmaWxlIjoiaG9tZS5qcyJ9
