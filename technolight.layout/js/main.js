478381e6bf140627b237a63d3557efa751c2aa68
Anar Agaev <anar.n.agaev@gmail.com>
2023-10-02 20:29:28 +0300

    Added animatin to the header comps of the Technolight layout

--- technolight.layout/js/main.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-// main.js
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8gbWFpbi5qc1xyXG4iXX0=
