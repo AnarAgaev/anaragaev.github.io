88974b9eabc5c48b45b538ce4bb5628a741f9742
Anar Agaev <anar.n.agaev@gmail.com>
2024-09-23 14:45:27 +0300

    Added Artelamp layout

---
+++ artelamp/fitting/include.js
@@ -0,0 +1,19 @@
+(function() {
+function add(type, value) {
+let element
+if(type === 'script') {
+element = document.createElement('script')
+element.type = 'module'
+element.crossorigin = true
+element.src = value
+}
+if(type === 'style') {
+element = document.createElement('link')
+element.rel = 'stylesheet'
+element.href = value
+}
+document.body.appendChild(element)
+}
+add('script','/assets/index-743507c2.js'); add('script','');
+add('style','/assets/index-ea5a1bee.css');
+})()
